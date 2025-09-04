b563074824a3bac6b2810ec45cf3674227189bba
Ado <minexdt@gmail.com>
2025-09-03 21:02:45 -0600

    Update _AllFake.js

--- plugins/_AllFake.js
+++ plugins/_AllFake.js
@@ -1,12 +1,12 @@
 import fs from 'fs'
 import path from 'path'
+import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
   try {
 
     let nombreBot = global.namebot || 'Bot'
     let bannerFinal = 'https://files.catbox.moe/wp5z1y.jpg'
-
 
     const botActual = conn.user?.jid?.split('@')[0].replace(/\D/g, '')
     const configPath = path.join('./JadiBots', botActual, 'config.json')
@@ -21,12 +21,26 @@ export async function before(m, { conn }) {
       }
     }
 
+    // Archivos falsos random
+    const docTypes = [
+      'pdf',
+      'zip',
+      'vnd.openxmlformats-officedocument.presentationml.presentation',
+      'vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'vnd.openxmlformats-officedocument.wordprocessingml.document'
+    ]
+    const document = docTypes[Math.floor(Math.random() * docTypes.length)]
+
+    const buffer = Buffer.from(`Archivo falso de ${document} generado por ${nombreBot}`)
 
     const canales = [global.idcanal, global.idcanal2]
     const newsletterJidRandom = canales[Math.floor(Math.random() * canales.length)]
 
-
     global.rcanal = {
+      document: buffer,
+      mimetype: `application/${document}`,
+      fileName: `🌾 𝖬𝗂𝖼𝗁𝗂 𝗦𝘂𝗯𝗕𝗼𝘁`,
+      fileLength: buffer.length,
       contextInfo: {
         isForwarded: true,
         forwardingScore: 1,
@@ -45,6 +59,7 @@ export async function before(m, { conn }) {
         }
       }
     }
+
   } catch (e) {
     console.log('Error al generar rcanal:', e)
   }
